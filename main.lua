d45f0fc9ff6ef6e1db905b6f275fc59895663499
vinicius-francisco-sm <viniciussiqueira166@gmail.com>
2025-08-20 20:47:10 -0300

    feat: refatora o código com utils + adiciona apresentação do monstro

--- main.lua
+++ main.lua
@@ -1,41 +1,23 @@
 -- Dependencias
 
+local utils = require("utils")
 local player = require("definitions.player")
 local colossus = require("definitions.colossus")
 
 -- Habilitar UTF-8 no terminal
 
-os.execute("chcp 65001")
+utils.enableUtf8()
 
 
 
 -- Header
 
-print([[
-=========================================================================
-
-                  _
-       _         | |
-      | | _______| |---------------------------------------------\
-      |:-)_______|==[]============================================>
-      |_|        | |---------------------------------------------/
-                 |_|
-
-                 ----------------------------------------
-
-                        ⚔️  SIMULADOR DE BATALHA ⚔️
-
-=========================================================================
-
-                Você empunha sua espada e se prepara para lutar.
-                                É hora da batalha!
-
-]])
-
--- Obter definição do jogador
-
-print(string.format("A vida do jogador é %d/%d", player.health, player.maxHealth))
+utils.printHeader()
 
 -- Obter definição do monstro
 
 local boss = colossus
+
+-- Apresentar o monstro
+
+utils.printCreature(boss)
---
+++ utils.lua
@@ -0,0 +1,74 @@
+local utils = {}
+
+---
+--- Habilita o modo UTF-8 no terminal, 
+--- para retornar carcteres acentuados e emojis.
+--- 
+function utils.enableUtf8()
+  os.execute("chcp 65001")
+end
+
+
+---
+--- Faz o print da apresentação do simulador no terminal.
+---
+function utils.printHeader()
+  print([[
+=========================================================================
+
+                  _
+       _         | |
+      | | _______| |---------------------------------------------\
+      |:-)_______|==[]============================================>
+      |_|        | |---------------------------------------------/
+                 |_|
+
+                 ----------------------------------------
+
+                        ⚔️  SIMULADOR DE BATALHA ⚔️
+
+=========================================================================
+
+                Você empunha sua espada e se prepara para lutar.
+                              É hora da batalha!
+
+]])
+end
+
+---
+---Calcula uma barra de progresso ASCII baseada em um atributo.
+---@param attribute number Número de 0 a 10.
+---@return string
+---
+function utils.getProgressBar(attribute)
+    local fullChar = "💀"
+    local emptyChar = "🖤"
+
+    local result = ""
+    for i = 1, 10, 1 do
+        result = result .. (i <= attribute and fullChar or emptyChar) -- funciona como operador ternário
+    end
+    return result
+end
+
+---
+---Faz o print das informações de uma criatura.
+---@param creature table
+---
+function utils.printCreature(creature)
+  -- calcula o indice de vida
+  local healthRate = math.floor((creature.health / creature.maxHealth) * 10)
+
+  -- cartão
+  print(" | " .. creature.name)
+  print(" | ")
+  print(" | " .. creature.description)
+  print(" | ")
+  print(" | Atributos")
+  print(" |    Vida:          " .. utils.getProgressBar(healthRate))
+  print(" |    Ataque:        " .. utils.getProgressBar(creature.attack))
+  print(" |    Defesa:        " .. utils.getProgressBar(creature.defense))
+  print(" |    Velocidade:    " .. utils.getProgressBar(creature.speed))
+end
+
+return utils
